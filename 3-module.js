163f34145e40a573089cc4b12a0c709bd68263ad
harika penukonda <penukondaharika@gmail.com>
2023-05-02 14:17:14 -0500

    Create 3-module.js

---
+++ 3-module.js
@@ -0,0 +1,16 @@
+// commonJS - every file is a module in node (by default)
+// Modules - Encapsulated Code (only share minimum)
+
+// To access them we need to use require
+
+const names = require('./4-names')
+// console.log(names) // { john: 'John', peter: 'Peter' }
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-addition') //The addition is 15
+
+//console.log(data) // { items: [ 'item1', 'item2' ], singlePerson: { name: 'bob' } }
+
+// sayHi('susan')
+// sayHi(names.john)
+// sayHi(names.peter)
