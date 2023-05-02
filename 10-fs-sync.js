78e2092c6d76068945bbd8dcd4a1a57a6539f6b0
harika penukonda <penukondaharika@gmail.com>
2023-05-02 15:19:18 -0500

    Create 10-fs-sync.js

---
+++ 10-fs-sync.js
@@ -0,0 +1,29 @@
+// fs.readFileSync(path[, options]) - Returns the contents of the path.
+
+// options <Object> | <string>
+// encoding <string> | <null> Default: null
+// flag <string> See support of file system flags. Default: 'r'.
+
+// fs.writeFileSync(file, data[, options])
+
+const {readFileSync, writeFileSync} = require('fs')
+
+const first = 
+    readFileSync(
+    './content/first.txt', // path
+    'utf8' // options - encoding code
+    ) 
+
+const second = 
+    readFileSync(
+        './content/second.txt',
+        'utf8'
+        )
+
+console.log(first, second)
+
+writeFileSync(
+    './content/returnSync.txt', // file
+    `Here is the result : ${first},${second}`, // data
+    {flag : 'a'} // option to append
+)
