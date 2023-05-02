8c697d5136048e39a3849f7b61b93fd9a86d5029
harika penukonda <penukondaharika@gmail.com>
2023-05-02 12:03:55 -0500

    Update app.js
    ReferenceError: sayHi is not defined

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // commonJS - every file is a module in node (by default)
 // Modules - Encapsulated Code (only share minimum)
 
-//
+// To access them we need to use require
 
-
+const names = require('./4-names')
+console.log(names) // { john: 'John', peter: 'Peter' }
 
 sayHi('susan')
 sayHi(john)
