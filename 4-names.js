0902e7752f115aa29893444456348a82de1617d6
harika penukonda <penukondaharika@gmail.com>
2023-05-02 12:02:04 -0500

    Update 4-names.js
    Update the code with exports object

--- 4-names.js
+++ 4-names.js
@@ -3,3 +3,9 @@ const secret = 'SUPER SECRET'
 // shareable
 const john = 'John'
 const peter = 'Peter'
+
+// console.log(module)  exports: {},
+
+// To make john and peter accessible from anywhere in our application
+
+module.exports = {john,peter} // we are using {} as there are 2 variable 
