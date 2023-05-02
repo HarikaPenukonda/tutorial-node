e67dab14b9c6ed95dda4cb5bf95cafadd266ad1e
harika penukonda <penukondaharika@gmail.com>
2023-05-02 11:55:40 -0500

    Create Intro file
    This is to show a basic code

---
+++ 2-globals.js
@@ -0,0 +1,14 @@
+// GLOBALS - NO WINDOW
+
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use module (file)
+// module - info about current module (file)
+// process - info about env where the program is being executed
+
+//console.log(__dirname); /Users/harikapenukonda/Desktop/tutorial
+//console.log(__filename); /Users/harikapenukonda/Desktop/tutorial/app.js
+
+setInterval(()=>{
+    console.log('Hello World')
+},1000)
---
+++ 4-names.js
@@ -0,0 +1,5 @@
+// local
+const secret = 'SUPER SECRET'
+// shareable
+const john = 'John'
+const peter = 'Peter'
