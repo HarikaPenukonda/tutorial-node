8266a6ee19c61cb80a47fedbb3e94713005ff8c2
harika penukonda <penukondaharika@gmail.com>
2023-05-02 12:01:38 -0500

    Update app.js
    Code is split amount 3 files

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 // commonJS - every file is a module in node (by default)
 // Modules - Encapsulated Code (only share minimum)
 
+//
 
 
-const sayHi = (name) => {
-    console.log(`Hello there ${name}`)
-}
 
 sayHi('susan')
 sayHi(john)
