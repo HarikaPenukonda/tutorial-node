e2a10ab94bee3f53ebaf0934cbc745f38501e73c
harika penukonda <penukondaharika@gmail.com>
2023-05-02 19:07:00 -0500

    Update 12-http-module.js
    Loading multiple pages

--- 12-http-module.js
+++ 12-http-module.js
@@ -1,8 +1,28 @@
+// const http = require('http')
+
+// const server = http.createServer((req,res)=>{
+//     res.write('Welcome to our home page!')
+//     res.end()
+// })
+
+// server.listen(5000)
+
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-    res.write('Welcome to our home page!')
-    res.end()
+    if(req.url === '/'){
+        res.end('Welcome to our home page!')
+    }
+    else if(req.url === '/about'){
+        res.end('Here is our short history')
+    }
+    else {
+        res.end(`<h1> Oops </h4>
+        <h4> we cannot seem to find the page you are looking for </h4>
+        <a href = "/"> Back </>`)
+    }
+    
+
 })
 
 server.listen(5000)
